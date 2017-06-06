7534d33b3de10d373024e9e47269925bafb76d95
Mateus Lai <mwlai@Mateuss-MacBook-Air.local>
2017-06-05 21:37:05 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,15 +1,23 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-var cardTwo = cards[2];
-cardsInPlay.push('cardOne');
-cardsInPlay.push('cardTwo');
 
-
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
-
-if (cardsInPlay.length === 2) {
-} if (cardsInPlay[0] === cardsInPlay[1]) {alert("You found a match!"); 
-} else if (cardsInPlay[0] !== cardsInPlay[1]) {alert("Sorry, try again.");
+var checkForMatch = function () {
+	if (cardsInPlay[0] ===[1]) {
+		console.log("You found a match!");
+	} else {
+		console.log("Sorry, try again"); }
 }
+
+var flipCard = function (cardId) {
+	console.log("User flipped " + cards[cardId]);
+	checkForMatch ();
+};
+
+flipCard(0);
+flipCard(2);
+
+
+
+
+
+
